c0199573a117308dbcf479232618023c1e721afd
サタン <64590881+HalfSatan@users.noreply.github.com>
2020-05-06 02:42:59 +0530

    Update dicedart.py

--- eleven/modules/dicedart.py
+++ eleven/modules/dicedart.py
@@ -11,8 +11,10 @@ from telegram.ext import run_async
 #eleven
 from eleven import dispatcher, updater, TOKEN, spamcheck
 from eleven.modules.disable import DisableAbleCommandHandler
+from eleven.modules.helper_funcs.chat_status import user_admin
 
 @run_async
+@user_admin
 @spamcheck
 def dice(update, context):
     chat = update.effective_chat
@@ -20,6 +22,7 @@ def dice(update, context):
     requests.get(url)
 
 @run_async
+@user_admin
 @spamcheck
 def darts(update, context):
     chat = update.effective_chat
