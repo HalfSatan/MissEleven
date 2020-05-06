8cd4d8a44cbac3df841ae3753295c1335261854a
サタン <64590881+HalfSatan@users.noreply.github.com>
2020-05-07 01:19:23 +0530

    Update reverse.py

--- eleven/modules/reverse.py
+++ eleven/modules/reverse.py
@@ -12,13 +12,14 @@ from telegram.ext import run_async
 
 from eleven import dispatcher
 from eleven.modules.disable import CommandHandler
+from eleven.modules.helper_funcs.chat_status import user_admin
 
 opener = urllib.request.build_opener()
 useragent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.38 Safari/537.36'
 # useragent = 'Mozilla/5.0 (Linux; Android 6.0.1; SM-G920V Build/MMB29K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/52.0.2743.98 Mobile Safari/537.36'
 opener.addheaders = [('User-agent', useragent)]
 
-
+@user_admin
 @run_async
 def reverse(update, context):
 	args = context.args
