828cfb66d8927e3d03bcd3beb59993e24638df1f
サタン <satanhalf07@gmail.com>
2020-05-09 03:37:25 +0530

    Update tts.py

--- eleven/modules/tts.py
+++ eleven/modules/tts.py
@@ -19,28 +19,28 @@ from eleven.modules.helper_funcs.extraction import extract_user
 
 def tts(update, context):
 	args = context.args
-    current_time = datetime.strftime(datetime.now(), "%d.%m.%Y %H:%M:%S")
-    filename = datetime.now().strftime("%d%m%y-%H%M%S%f")
-    reply = " ".join(args)
-    update.message.chat.send_action(ChatAction.RECORD_AUDIO)
-    lang="ml"
-    tts = gTTS(reply, lang)
-    tts.save("k.mp3")
-    with open("k.mp3", "rb") as f:
-        linelist = list(f)
-        linecount = len(linelist)
-    if linecount == 1:
-        update.message.chat.send_action(ChatAction.RECORD_AUDIO)
-        lang = "en"
-        tts = gTTS(reply, lang)
-        tts.save("k.mp3")
-    with open("k.mp3", "rb") as speech:
-        update.message.reply_voice(speech, quote=False)
+	current_time = datetime.strftime(datetime.now(), "%d.%m.%Y %H:%M:%S")
+	filename = datetime.now().strftime("%d%m%y-%H%M%S%f")
+	reply = " ".join(args)
+	update.message.chat.send_action(ChatAction.RECORD_AUDIO)
+	lang="ml"
+	tts = gTTS(reply, lang)
+	tts.save("k.mp3")
+	with open("k.mp3", "rb") as f:
+		linelist = list(f)
+		linecount = len(linelist)
+	if linecount == 1:
+		update.message.chat.send_action(ChatAction.RECORD_AUDIO)
+		lang = "en"
+		tts = gTTS(reply, lang)
+		tts.save("k.mp3")
+	with open("k.mp3", "rb") as speech:
+		update.message.reply_voice(speech, quote=False)
 
 
-    __help__ = """ Text to speech
-    - /tts <your text>
-    """
-    __mod_name__ = "tts"
+	__help__ = """ Text to speech
+	- /tts <your text>
+	"""
+	__mod_name__ = "tts"
 
 dispatcher.add_handler(CommandHandler('tts', tts, pass_args=True))
