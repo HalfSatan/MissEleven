e2abacd2dacfbd81bc9f0d6d8c2b2489b8082be9
サタン <64590881+HalfSatan@users.noreply.github.com>
2020-05-19 02:08:59 +0530

    Update disable.py

--- eleven/modules/disable.py
+++ eleven/modules/disable.py
@@ -257,7 +257,7 @@ if is_module_loaded(FILENAME):
         return build_curr_disabled(chat_id)
 
 
-    __mod_name__ = "Command disabling"
+    __mod_name__ = "Disabling"
 
     __help__ = "disable_help"
 
