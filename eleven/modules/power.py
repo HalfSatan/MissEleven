0214db26253bbcfe70e339a0db6bb8d4df565fdb
サタン <64590881+HalfSatan@users.noreply.github.com>
2020-05-22 13:27:17 +0530

    Update power.py

--- eleven/modules/power.py
+++ eleven/modules/power.py
@@ -4,11 +4,12 @@ import os
 from typing import List, Optional
 
 from telegram import Bot, Update, ParseMode, TelegramError
-from telegram.ext import CommandHandler, run_async
+from telegram.ext import CommandHandler, run_async, Filters
 from telegram.utils.helpers import mention_html
 
 from eleven import dispatcher, WHITELIST_USERS, SUPPORT_USERS, SUDO_USERS, OWNER_ID
 from eleven.modules.helper_funcs.extraction import extract_user
+from eleven.modules.helper_funcs.filters import CustomFilters
 
 ELEVATED_USERS_FILE = os.path.join(os.getcwd(), 'eleven/elevated_users.json')
 
