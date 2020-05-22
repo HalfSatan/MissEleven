0f194814f7b3876dd9db7e5c5b3fc40c41cac0cd
サタン <64590881+HalfSatan@users.noreply.github.com>
2020-05-22 05:50:30 +0530

    Create power.py

---
+++ eleven/modules/power.py
@@ -0,0 +1,316 @@
+import html
+import json
+import os
+from typing import List, Optional
+
+from telegram import Bot, Update, ParseMode, TelegramError
+from telegram.ext import CommandHandler, run_async
+from telegram.utils.helpers import mention_html
+
+from tg_bot import dispatcher, WHITELIST_USERS, SUPPORT_USERS, SUDO_USERS, OWNER_ID
+from tg_bot.modules.helper_funcs.extraction import extract_user
+from tg_bot.modules.log_channel import gloggable
+
+ELEVATED_USERS_FILE = os.path.join(os.getcwd(), 'eleven/elevated_users.json')
+
+
+def check_user_id(user_id: int, bot: Bot) -> Optional[str]:
+    if not user_id:
+        reply = "That...is a chat! You Moron?"
+
+    elif user_id == bot.id:
+        reply = "This does not work that way."
+
+    else:
+        reply = None
+    return reply
+
+@run_async
+def addsudo(update, context) -> str:
+    args = context.args
+    message = update.effective_message
+    user = update.effective_user
+    chat = update.effective_chat
+
+    user_id = extract_user(message, args)
+    user_member = bot.getChat(user_id)
+    rt = ""
+
+    reply = check_user_id(user_id, bot)
+    if reply:
+        message.reply_text(reply)
+        return ""
+
+    with open(ELEVATED_USERS_FILE, 'r') as infile:
+        data = json.load(infile)
+
+    if user_id in SUDO_USERS:
+        message.reply_text("This member is already a Sudo User")
+        return ""
+
+    if user_id in SUPPORT_USERS:
+        rt += "Requested HA to promote a Support User to Sudo User."
+        data['supports'].remove(user_id)
+        SUPPORT_USERS.remove(user_id)
+
+    if user_id in WHITELIST_USERS:
+        rt += "Requested HA to promote a Whitelist User to Sudo User."
+        data['whitelists'].remove(user_id)
+        WHITELIST_USERS.remove(user_id)
+
+    data['sudos'].append(user_id)
+    SUDO_USERS.append(user_id)
+
+    with open(ELEVATED_USERS_FILE, 'w') as outfile:
+        json.dump(data, outfile, indent=4)
+
+    update.effective_message.reply_text(
+        rt + "\nSuccessfully set Power level of {} to Sudo User!".format(user_member.first_name))
+
+    log_message = (f"#SUDO\n"
+                   f"<b>Admin:</b> {mention_html(user.id, user.first_name)}\n"
+                   f"<b>User:</b> {mention_html(user_member.id, user_member.first_name)}")
+
+    if chat.type != 'private':
+        log_message = f"<b>{html.escape(chat.title)}:</b>\n" + log_message
+
+    return log_message
+
+
+@run_async
+def addsupport(update, context) -> str:
+    args = context.args
+    message = update.effective_message
+    user = update.effective_user
+    chat = update.effective_chat
+
+    user_id = extract_user(message, args)
+    user_member = bot.getChat(user_id)
+    rt = ""
+
+    reply = check_user_id(user_id, bot)
+    if reply:
+        message.reply_text(reply)
+        return ""
+
+    with open(ELEVATED_USERS_FILE, 'r') as infile:
+        data = json.load(infile)
+
+    if user_id in SUDO_USERS:
+        rt += "Requested HA to deomote this Sudo User to Support User"
+        data['sudos'].remove(user_id)
+        SUDO_USERS.remove(user_id)
+
+    if user_id in SUPPORT_USERS:
+        message.reply_text("This user is already a Support User.")
+        return ""
+
+    if user_id in WHITELIST_USERS:
+        rt += "Requested HA to promote this Whitelist User to Support User"
+        data['whitelists'].remove(user_id)
+        WHITELIST_USERS.remove(user_id)
+
+    data['supports'].append(user_id)
+    SUPPORT_USERS.append(user_id)
+
+    with open(ELEVATED_USERS_FILE, 'w') as outfile:
+        json.dump(data, outfile, indent=4)
+
+    update.effective_message.reply_text(rt + f"\n{user_member.first_name} was added as a Support User!")
+
+    log_message = (f"#SUPPORT\n"
+                   f"<b>Admin:</b> {mention_html(user.id, user.first_name)}\n"
+                   f"<b>User:</b> {mention_html(user_member.id, user_member.first_name)}")
+
+    if chat.type != 'private':
+        log_message = f"<b>{html.escape(chat.title)}:</b>\n" + log_message
+
+    return log_message
+
+
+@run_async
+def addwhitelist(update, context) -> str:
+    args = context.args
+    message = update.effective_message
+    user = update.effective_user
+    chat = update.effective_chat
+
+    user_id = extract_user(message, args)
+    user_member = bot.getChat(user_id)
+    rt = ""
+
+    reply = check_user_id(user_id, bot)
+    if reply:
+        message.reply_text(reply)
+        return ""
+
+    with open(ELEVATED_USERS_FILE, 'r') as infile:
+        data = json.load(infile)
+
+    if user_id in SUDO_USERS:
+        rt += "This member is a Sudo User, Demoting to Whitelist User."
+        data['sudos'].remove(user_id)
+        SUDO_USERS.remove(user_id)
+
+    if user_id in SUPPORT_USERS:
+        rt += "This user is already a Demon Support User, Demoting to Whitelist User."
+        data['supports'].remove(user_id)
+        SUPPORT_USERS.remove(user_id)
+
+    if user_id in WHITELIST_USERS:
+        message.reply_text("This user is already a Whitelist User.")
+        return ""
+
+    data['whitelists'].append(user_id)
+    WHITELIST_USERS.append(user_id)
+
+    with open(ELEVATED_USERS_FILE, 'w') as outfile:
+        json.dump(data, outfile, indent=4)
+
+    update.effective_message.reply_text(
+        rt + f"\nSuccessfully promoted {user_member.first_name} to Whitelist User!")
+
+    log_message = (f"#WHITELIST\n"
+                   f"<b>Admin:</b> {mention_html(user.id, user.first_name)} \n"
+                   f"<b>User:</b> {mention_html(user_member.id, user_member.first_name)}")
+
+    if chat.type != 'private':
+        log_message = f"<b>{html.escape(chat.title)}:</b>\n" + log_message
+
+    return log_message
+
+
+@run_async
+def removesudo(update, context) -> str:
+    args = context.args
+    message = update.effective_message
+    user = update.effective_user
+    chat = update.effective_chat
+
+    user_id = extract_user(message, args)
+    user_member = bot.getChat(user_id)
+
+    reply = check_user_id(user_id, bot)
+    if reply:
+        message.reply_text(reply)
+        return ""
+
+    with open(ELEVATED_USERS_FILE, 'r') as infile:
+        data = json.load(infile)
+
+    if user_id in SUDO_USERS:
+        message.reply_text("Requested HA to demote this user to Civilian")
+        SUDO_USERS.remove(user_id)
+        data['sudos'].remove(user_id)
+
+        with open(ELEVATED_USERS_FILE, 'w') as outfile:
+            json.dump(data, outfile, indent=4)
+
+        log_message = (f"#UNSUDO\n"
+                       f"<b>Admin:</b> {mention_html(user.id, user.first_name)}\n"
+                       f"<b>User:</b> {mention_html(user_member.id, user_member.first_name)}")
+
+        if chat.type != 'private':
+            log_message = "<b>{}:</b>\n".format(html.escape(chat.title)) + log_message
+
+        return log_message
+
+    else:
+        message.reply_text("This user is not a Sudo User!")
+        return ""
+
+
+@run_async
+def removesupport(update, context) -> str:
+    args = context.args
+    message = update.effective_message
+    user = update.effective_user
+    chat = update.effective_chat
+
+    user_id = extract_user(message, args)
+    user_member = bot.getChat(user_id)
+
+    reply = check_user_id(user_id, bot)
+    if reply:
+        message.reply_text(reply)
+        return ""
+
+    with open(ELEVATED_USERS_FILE, 'r') as infile:
+        data = json.load(infile)
+
+    if user_id in SUPPORT_USERS:
+        message.reply_text("Requested HA to demote this user to Civilian")
+        SUPPORT_USERS.remove(user_id)
+        data['supports'].remove(user_id)
+
+        with open(ELEVATED_USERS_FILE, 'w') as outfile:
+            json.dump(data, outfile, indent=4)
+
+        log_message = (f"#UNSUPPORT\n"
+                       f"<b>Admin:</b> {mention_html(user.id, user.first_name)}\n"
+                       f"<b>User:</b> {mention_html(user_member.id, user_member.first_name)}")
+
+        if chat.type != 'private':
+            log_message = f"<b>{html.escape(chat.title)}:</b>\n" + log_message
+
+        return log_message
+
+    else:
+        message.reply_text("This user is not a Support User!")
+        return ""
+
+
+@run_async
+def removewhitelist(update, context) -> str:
+    args = context.args
+    message = update.effective_message
+    user = update.effective_user
+    chat = update.effective_chat
+
+    user_id = extract_user(message, args)
+    user_member = bot.getChat(user_id)
+
+    reply = check_user_id(user_id, bot)
+    if reply:
+        message.reply_text(reply)
+        return ""
+
+    with open(ELEVATED_USERS_FILE, 'r') as infile:
+        data = json.load(infile)
+
+    if user_id in WHITELIST_USERS:
+        message.reply_text("Demoting to normal user")
+        WHITELIST_USERS.remove(user_id)
+        data['whitelists'].remove(user_id)
+
+        with open(ELEVATED_USERS_FILE, 'w') as outfile:
+            json.dump(data, outfile, indent=4)
+
+        log_message = (f"#UNWHITELIST\n"
+                       f"<b>Admin:</b> {mention_html(user.id, user.first_name)}\n"
+                       f"<b>User:</b> {mention_html(user_member.id, user_member.first_name)}")
+
+        if chat.type != 'private':
+            log_message = f"<b>{html.escape(chat.title)}:</b>\n" + log_message
+
+        return log_message
+    else:
+        message.reply_text("This user is not a Whitelist User!")
+        return ""
+
+
+SUDO_HANDLER = CommandHandler(addsudo, addsudo, pass_args=True)
+SUPPORT_HANDLER = CommandHandler(addsupport, addsupport, pass_args=True)
+WHITELIST_HANDLER = CommandHandler(addwhitelist, addwhitelist, pass_args=True)
+
+UNSUDO_HANDLER = CommandHandler(removesudo, removesudo, pass_args=True)
+UNSUPPORT_HANDLER = CommandHandler(removesupport, removesupport, pass_args=True)
+UNWHITELIST_HANDLER = CommandHandler(removewhitelist, removewhitelist, pass_args=True)
+
+dispatcher.add_handler(SUDO_HANDLER)
+dispatcher.add_handler(SUPPORT_HANDLER)
+dispatcher.add_handler(WHITELIST_HANDLER)
+
+dispatcher.add_handler(UNSUDO_HANDLER)
+dispatcher.add_handler(UNSUPPORT_HANDLER)
+dispatcher.add_handler(UNWHITELIST_HANDLER)
